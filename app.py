f36f2bd42628b5d968d9694420af7976875a6e2e
silviolima07 <silviolima07@gmail.com>
2022-01-30 13:14:20 -0300

    Teste

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def main():
         
         colunas = list(df_tab6_1.columns)
         
-        df_tab6_1.loc[df_tab6_1['Grupamento de Atividades Econômicas e Seção CNAE 2.0'] != "---"])
+        df_tab6_1.loc[df_tab6_1['Grupamento de Atividades Econômicas e Seção CNAE 2.0'] != "---"]
         
         
         st.table(df_tab6_1[colunas][:27])
