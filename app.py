e53f8f770e2a26b0fe77aeae3b53a5b02d71416b
silviolima07 <silviolima07@gmail.com>
2022-01-30 19:40:11 -0300

    TESTE

--- app.py
+++ app.py
@@ -128,6 +128,8 @@ def main():
     
     filtro_mes_ano = opcao_mes+'/'+opcao_ano
     
+    filtro_mes_ano = 'Janeiro/2020'
+    
         
     colunas = ['Grupamento de Atividades Econômicas e Seção CNAE 2.0', filtro_mes_ano]
     
