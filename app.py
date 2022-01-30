6e9d80b5eb1e30144659ce4d8426916f94dc7b23
silviolima07 <silviolima07@gmail.com>
2022-01-30 13:49:01 -0300

    Teste

--- app.py
+++ app.py
@@ -150,7 +150,8 @@ def main():
         df_teste = pd.read_csv('./caged.csv')
         
         df_teste = df_teste.loc[df_teste['Grupamento de Atividades Econômicas e Seção CNAE 2.0'] != 'Não identificado***']
-        
+        df_teste = df_teste.loc[df_teste['Grupamento de Atividades Econômicas e Seção CNAE 2.0'] != 'Total']
+
         st.table(df_teste)
         
         st.table(df_tab6_1[colunas][1:27])
