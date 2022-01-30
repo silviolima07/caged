c3abe57a9ea1251a7f573694492bc60da8296723
silviolima07 <silviolima07@gmail.com>
2022-01-30 12:24:04 -0300

    Filtro

--- app.py
+++ app.py
@@ -108,6 +108,8 @@ def main():
     
     df_tab6.rename(columns={'Unnamed: 1_level_1':"Grupamento de Atividades Econômicas e Seção CNAE 2.0"}, inplace=True)
     
+    df_tab6.columns = df_tab6.columns.droplevel(0)
+    
     #df_tab6.drop(columns=['Unnamed: 1_level_1'], level=1, inplace=True)
    
     df_tab6_1 = df_tab6[colunas].dropna()
