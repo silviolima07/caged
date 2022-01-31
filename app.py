1397199e4b9793689828c323d3fe394ea823b1e6
silviolima07 <silviolima07@gmail.com>
2022-01-31 20:48:44 -0300

    teste

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def main():
     
     st.table(df_tab6_1) # TESTE
     
-    df_tab6_1.to_csv("caged.csv", index=False, encoding='utf-8)
+    df_tab6_1.to_csv("caged.csv", index=False, encoding='utf-8')
     
     with open('caged.csv', encoding="utf-8") as f:
         nome = 'caged_'+opcao_mes+'_'+str(opcao_ano)+'.csv'
