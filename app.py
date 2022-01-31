3d78f18df063ce1e1a99cab41f2ce7e65fdc06b5
silviolima07 <silviolima07@gmail.com>
2022-01-31 16:19:44 -0300

    teste

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ def main():
     
     df_tab6 = excel_to_pandas2(url_tabela,'caged.xlsx', 'Tabela 6', [4,5] )
     
-    st.table(df_tab6)
+    st.table(df_tab6[:27])
     """
     df_teste = df_tab6.dropna()
     teste_colunas = df_teste.columns
