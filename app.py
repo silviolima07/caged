f22887ad9d9df921d6615cb2a4b569b6a3a92137
silviolima07 <silviolima07@gmail.com>
2022-01-31 16:13:32 -0300

    teste

--- app.py
+++ app.py
@@ -153,9 +153,9 @@ def main():
     
     
     html_page3 = """
-    <div style="background-color:blue;padding=40px">
-        <p style='text-align:center;font-size:40px;font-weight:bold;color:red'>Relatório</p>
-    </div>
+    #<div style="background-color:blue;padding=40px">
+    #    <p style='text-align:center;font-size:40px;font-weight:bold;color:red'>Relatório</p>
+    #</div>
               """
     st.markdown(html_page3, unsafe_allow_html=True)
     
