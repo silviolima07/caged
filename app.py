d813bb8da68e337621c2dde2247ddf863ca6b535
silviolima07 <silviolima07@gmail.com>
2022-02-02 20:36:40 -0300

    teste

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def main():
     
         df_tab6_1.to_csv("caged.csv", index=False, encoding='ISO-8859-1')
         
-        df = pd.read_csv("caged.csv",decimal=',', encoding='ISO-8859-1', dtype= {'Estoque':'int',	'Admissões':'int',	'Desligamentos':'int',	'Saldos':'int'})
+        df = pd.read_csv("caged.csv",decimal=',', encoding='ISO-8859-1', dtype= {'Estoque':'string',	'Admissões':'string',	'Desligamentos':'string',	'Saldos':'string'})
         
         #df[coluna_float] = round(df[coluna_float],2)
         
