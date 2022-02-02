04fdba003273756582cdf18364ae48ab97494491
silviolima07 <silviolima07@gmail.com>
2022-02-01 23:02:13 -0300

    st.dataframe

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def main():
     
         #st.write(df_tab6_1.columns)
         
-        st.dataframe(df_tab6_1.style.format(subset=['Position', 'Marks'], formatter="{:.2f}"
+        st.dataframe(df_tab6_1.style.format(subset=['Position', 'Marks'], formatter="{:.2f}"))
     
         st.table(df_tab6_1)
     
