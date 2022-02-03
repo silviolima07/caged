5ab87ab8cc737652229dff8b880c0f15e138b37f
silviolima07 <silviolima07@gmail.com>
2022-02-02 21:15:24 -0300

    teste

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def main():
         
         #df = df_tab6_1.style.format({"Expense": lambda x : '{:}'.format(x)})
         
-        st.dataframe(df_tab6_1.style.background_gradient(cmap=‘Greens’).set_precision(2),height=500)
+        st.dataframe(df_tab6_1.style.background_gradient(cmap='Greens').set_precision(2),height=500)
         
         #st.table(df)
         
